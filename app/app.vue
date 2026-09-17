4d282afd279f33866f221251bd8c54d6b9a74d84
Csituka_D <694428809@qq.com>
2026-09-17 12:31:00 +0800

    fix

--- app/app.vue
+++ app/app.vue
@@ -1,14 +1,6 @@
 <template>
-  <v-app>
-    <NuxtRouteAnnouncer />
-    <AppHeader />
-
-    <v-main>
-      <v-container>
-        <div class="text-h4">
-          sample
-        </div>
-      </v-container>
-    </v-main>
-  </v-app>
+  <NuxtRouteAnnouncer />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-container>
+    <div class="text-h4">
+      sample
+    </div>
+  </v-container>
+</template>
